1a1723ed7722ab21ff610aa4b374a7008b504e3d
Helioripple <studio.helioripple@gmail.com>
2026-05-11 23:02:46 +0330

    root

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DB_USER = os.getenv("DB_USER","server")
+    DB_USER = os.getenv("DB_USER","root")
     DB_PASSWORD = os.getenv("DB_PASSWORD","rE1IY89Me3kLBTWwtW-S")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "3306")
