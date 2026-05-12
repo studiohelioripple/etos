ea1423459bdb8145b4f798645462fec1506c6159
Amin Bahrami <xauses@gmail.com>
2026-05-12 14:57:34 +0330

    step 1

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 class Config:
     DB_USER = os.getenv("DB_USER","root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD","rE1IY89Me3kLBTWwtW-S")
+    DB_PASSWORD = os.getenv("DB_PASSWORD","")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "3306")
-    DB_NAME = os.getenv("DB_NAME","elequa")
+    DB_NAME = os.getenv("DB_NAME","amens")
 
     SQLALCHEMY_DATABASE_URL = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
