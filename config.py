93ffd4064a2a0987299a2f4a7ee0571c79b047e3
Amin Bahrami <xauses@gmail.com>
2026-05-12 16:48:00 +0330

    127.0.01

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     DB_USER = os.getenv("DB_USER","root")
     DB_PASSWORD = os.getenv("DB_PASSWORD","")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
     DB_PORT = os.getenv("DB_PORT", "3306")
     DB_NAME = os.getenv("DB_NAME","amens")
 
